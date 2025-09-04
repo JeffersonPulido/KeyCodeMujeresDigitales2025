acdb1241799965ea2be843ac4c3f4f21bcdad254
JeffersonPulido <jeffersonpulido55@gmail.com>
2025-09-03 20:37:20 -0500

    Teoria estructuras de control js

---
+++ Ejercicios/teoria.js
@@ -0,0 +1,49 @@
+// // If - Else
+
+// console.log("=== IF - ELSE  ===");
+// const numero = 10;
+
+// if (isNaN(numero)) {
+//     console.log("Entrada invalida, no es un numero");
+// } else if (numero % 2 === 0) {
+//     console.log("Es par");
+// } else {
+//     console.log("Es impar");
+// }
+
+// // Switch
+// console.log("=== SWITCH  ===");
+
+// const option = 32;
+
+// switch (option) {
+//     case 1:
+//         console.log("Medicina general");
+//         break;
+//     case 2:
+//         console.log("Odontologia");
+//         break;
+//     case 3:
+//         console.log("Psicologia");
+//         break;
+//     default:
+//         console.log("Esa opcion no existe en este hospital");
+//         break;
+// }
+
+// // For
+// console.log("=== FOR  ===");
+
+// for (let index = 0; index <= 5; index++) {
+//     console.log(`Vuelta numero: ${index}`);
+// }
+
+// // While
+// console.log("=== WHILE  ===");
+
+// let contador = 1;
+
+// while (contador <= 10) {
+//     console.log(`Vamos en el numero: ${contador}`)
+//     contador++;
+// }
