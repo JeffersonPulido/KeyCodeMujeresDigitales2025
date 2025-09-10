4fbcdb5dc95f6a28851ef7eb03726f62a18061b2
JeffersonPulido <jeffersonpulido55@gmail.com>
2025-09-10 18:36:45 -0500

    Adicion de teoria compuertas logicas

--- Ejercicios/teoria.js
+++ Ejercicios/teoria.js
@@ -110,3 +110,19 @@ if (age > 12) {
 }
 
 age > 12 ? console.log('Mayor de edad') : console.log("Menor de edad")
+
+// DIFERENCIAS
+
+//Operador ternario
+//condicion ? caso positivo : caso negativo
+
+// Puerta logica OR - se cumple uno u otro
+//usuario === admin || usuario === vendedor
+
+// Operador de fusion nula - Si el valor de la izquierda existe, se usa, sino existe entonces se us
+// el valor de la derecha
+//usuario.nombre ?? 'valor por defecto'
+
+// Puerta logica AND - Si el valor de la izquierda es verdadero entonces se imprime o se usa el valor
+// de la derecha
+//usuario && Renderizo pagina privada
