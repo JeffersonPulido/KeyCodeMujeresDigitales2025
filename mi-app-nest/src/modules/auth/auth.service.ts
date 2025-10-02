4d243de9ade07078580c0cfb525aa658e5c393cc
JeffersonPulido <jeffersonpulido55@gmail.com>
2025-10-01 20:40:53 -0500

    [FEAT] TypeORM Conexion BD y migraciones

--- mi-app-nest/src/modules/auth/auth.service.ts
+++ mi-app-nest/src/modules/auth/auth.service.ts
@@ -6,17 +6,17 @@ import { LoginDTO } from 'src/dto/login.dto';
 export class AuthService {
     constructor(private readonly usersService: UsersService) {}
 
-    login(data: LoginDTO) {
-        const users = this.usersService.findAll();
-        const user = users.find(user => user.email === data.email && user.password === data.password)
+    // login(data: LoginDTO) {
+    //     const users = this.usersService.findAll();
+    //     const user = users.find(user => user.email === data.email && user.password === data.password)
 
-        if (!user) {
-            throw new UnauthorizedException("Credenciales invalidas");
-        }
+    //     if (!user) {
+    //         throw new UnauthorizedException("Credenciales invalidas");
+    //     }
 
-        return {
-            user: { id: user.id, name: user.name, email: user.email },
-            accessToken: `fake-token-${user.id}-${Date.now()}`
-        }
-    }
+    //     return {
+    //         user: { id: user.id, name: user.name, email: user.email },
+    //         accessToken: `fake-token-${user.id}-${Date.now()}`
+    //     }
+    // }
 }
