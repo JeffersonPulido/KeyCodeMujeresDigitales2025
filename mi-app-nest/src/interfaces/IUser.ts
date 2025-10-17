309b91072943de8c3b05a911d3113fda7d096a1e
JeffersonPulido <jeffersonpulido55@gmail.com>
2025-10-16 20:58:12 -0500

    [TEST] UsersController

--- mi-app-nest/src/interfaces/IUser.ts
+++ mi-app-nest/src/interfaces/IUser.ts
@@ -2,9 +2,9 @@ import { Roles } from "src/entities/user.entity";
 
 export type IUser = {
     id: number,
-    name: string,
-    email: string,
-    password: string,
+    name?: string,
+    email?: string,
+    password?: string,
     age?: number,
     role: Roles,
 }
